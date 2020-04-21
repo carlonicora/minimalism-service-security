c0b8460f8a022a567ea3efd3542ead7fb6eaa064
skuzminich <8dJkzdHGpxxn>
2020-04-21 03:01:50 +0300

    Session expired expression should be called with code only

--- src/exceptions/sessionExpiredException.php
+++ src/exceptions/sessionExpiredException.php
@@ -2,7 +2,15 @@
 namespace carlonicora\minimalism\services\security\exceptions;
 
 use RuntimeException;
+use Throwable;
 
 class sessionExpiredException extends RuntimeException {
-
+    /**
+     * entityNotFoundException constructor.
+     * @param string $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(string $code, Throwable $previous = null) {
+        parent::__construct('Session expired', $code, $previous);
+    }
 }
